69dd67ab5710e77ff2649cbc5d681fb1b01478f7
Gab <gbl_machado@hotmail.com>
2024-02-06 20:46:49 -0300

    updating components folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">GA-Blog</span>
+      </div>
+    </nav>
+  );
+};
+
